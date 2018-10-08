71bb3d311a2de8ff9a77a4507f4691e953f17ebc
sayeed99 <sayeed.afridi2009@gmail.com>
2018-10-08 13:17:52 +0530

    Build : update anykernel

--- out/anykernel.sh
+++ out/anykernel.sh
@@ -4,46 +4,30 @@
 ## AnyKernel setup
 # begin properties
 properties() {
-kernel.string=Flare-O
+kernel.string=FlareP
 do.devicecheck=1
-do.modules=1
+do.modules=0
 do.cleanup=1
-do.cleanuponabort=1
-device.name1=tissot
-device.name2=Mi A1
-device.name3=tissot_sprout
+do.cleanuponabort=0
+do.system_blobs=0
+device.name1=ASUS_X00TD
+device.name2=X00TD
+device.name3=
+device.name4=
+device.name5=
 } # end properties
 
 # shell variables
-block=/dev/block/platform/soc/7824900.sdhci/by-name/boot;
-is_slot_device=1;
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
 ramdisk_compression=auto;
-
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . /tmp/anykernel/tools/ak2-core.sh;
 
-
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-chmod -R 750 $ramdisk/*;
-chown -R root:root $ramdisk/*;
-
-
 ## AnyKernel install
 dump_boot;
-
-if [ -d $ramdisk/.subackup -o -d $ramdisk/.backup ]; then
-  patch_cmdline "skip_override" "skip_override";
-else
-  patch_cmdline "skip_override" "";
-fi;
-
-# begin ramdisk changes
-
-
-# end ramdisk changes
 
 write_boot;
 
